276d7c8a0cc4665cb0cb025dce00649e03aafbfe
Zsofia Voros <zsofiav1@gmail.com>
2019-04-24 15:53:03 -0400

    Added Animation and Backgrokground Assets

--- Cell Biology/Assets/Scripts/PlayerController.cs
+++ Cell Biology/Assets/Scripts/PlayerController.cs
@@ -9,17 +9,21 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     public int length;
-    private float height = 6.579999f;
     private Rigidbody2D rb;
     private Vector2 moveVelocity; 
     private SpriteRenderer sprite_renderer;
     private BoxCollider2D box_collider;
+    private float height;
+    private float width;
 
     void Awake() {
+
         sprite_renderer = GetComponent<SpriteRenderer>();
         box_collider = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
-    }
+        height = 6.911709f;
+        width = 7.51125f;
+}
 
     // Start is called before the first frame update
     void Start()
@@ -27,8 +31,8 @@ public class PlayerController : MonoBehaviour
         //sprite_renderer = GetComponent<SpriteRenderer>();
         //box_collider = GetComponent<BoxCollider2D>();
         //rb = GetComponent<Rigidbody2D>();
-        sprite_renderer.size = new Vector2(length * 6.55f, height);
-        box_collider.size = new Vector2(length * 6.55f, height);
+        sprite_renderer.size = new Vector2(length * width, height);
+        box_collider.size = new Vector2(length * width, height);
     }
 
     // Update is called once per frame
@@ -45,16 +49,16 @@ public class PlayerController : MonoBehaviour
 
     public void SetLength(int l) {
         length = l;
-        sprite_renderer.size = new Vector2(length * 6.55f, height);
-        box_collider.size = new Vector2(length * 6.55f, height);
+        sprite_renderer.size = new Vector2(length * width, height);
+        box_collider.size = new Vector2(length * width, height);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Enemy")){
             if (length > 1) {
                 length = length - 1;
-                sprite_renderer.size = new Vector2(length * 6.55f, height);
-                box_collider.size = new Vector2(length * 6.55f, height);
+                sprite_renderer.size = new Vector2(length * width, height);
+                box_collider.size = new Vector2(length * width, height);
             }
         }
     }
