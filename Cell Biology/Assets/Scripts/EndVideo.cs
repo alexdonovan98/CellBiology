db86c57e1a6828bded4f42ac15759acb3d7780c7
alexdonovan98 <alex98@live.unc.edu>
2019-05-06 18:04:46 -0400

    Final score working

--- Cell Biology/Assets/Scripts/EndVideo.cs
+++ Cell Biology/Assets/Scripts/EndVideo.cs
@@ -10,14 +10,13 @@ public class EndVideo : MonoBehaviour
 {
     public float timer = 48;
 
-    // Update is called once per frame
     void Update()
     {
         timer -= Time.deltaTime;    
 
         if(timer <= 0)
         {
-            SceneManager.LoadScene("Pre-Tutorial");
+            SceneManager.LoadScene("Video");
         }
     }
 }
--- Cell Biology/Assets/Scripts/FinalScore.cs
+++ Cell Biology/Assets/Scripts/FinalScore.cs
@@ -8,13 +8,13 @@ using UnityEngine.UI;
 public class FinalScore : MonoBehaviour
 {
     public Text finalScore;
-
+    public Text qScoreSum;
+    public Text damage;
+   
     void Start()
     {
-        //if ((GlobalControl.Instance.Q1Score + GlobalControl.Instance.Q2Score + GlobalControl.Instance.Q3Score + GlobalControl.Instance.Q4Score + GlobalControl.Instance.Q5Score) - GlobalControl.Instance.totalDamage) <= 0) {
-          //  finalScore.text = "Your final score is 0";
-        //} else {
-        finalScore.text = "Your final score is " + ((GlobalControl.Instance.Q1Score + GlobalControl.Instance.Q2Score + GlobalControl.Instance.Q3Score + GlobalControl.Instance.Q4Score + GlobalControl.Instance.Q5Score) - GlobalControl.Instance.totalDamage).ToString();
-        //}
+        qScoreSum.text = "Sum of question scores: " + (GlobalControl.Instance.Q1Score + GlobalControl.Instance.Q2Score + GlobalControl.Instance.Q3Score + GlobalControl.Instance.Q4Score + GlobalControl.Instance.Q5Score).ToString();
+        damage.text = "- Damage penalty: " + GlobalControl.Instance.totalDamage.ToString();
+        finalScore.text = "Your final score is: " + ((GlobalControl.Instance.Q1Score + GlobalControl.Instance.Q2Score + GlobalControl.Instance.Q3Score + GlobalControl.Instance.Q4Score + GlobalControl.Instance.Q5Score) - GlobalControl.Instance.totalDamage).ToString();
     }
 }
